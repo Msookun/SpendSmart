6afd99e790533335f81c528df536ef93ea60cd2e
Msookun <sookunmm@gmail.com>
2025-06-23 18:53:43 +0400

    Add project files.

---
+++ SpendSmart/Models/Expense.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendSmart.Models
+{
+    public class Expense
+    {
+        public int id {  get; set; }
+        public decimal Value { get; set; }
+
+        [Required]
+        public string? Description {  get; set; }
+    }
+}
